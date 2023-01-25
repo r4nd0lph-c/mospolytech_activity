36528338f06fd4946380ee1ad8c88453a1027f52
r4nd0lph <r11345roma@yandex.ru>
2023-01-25 18:04:51 +0300

    added models & its admin representation

--- activity/main/admin.py
+++ activity/main/admin.py
@@ -1,7 +1,78 @@
 from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
+from admin_auto_filters.filters import AutocompleteFilter
+
+from main.models import *
 from main.views import Auth, logout_user
+
+
+class StudyGroupFilter(AutocompleteFilter):
+    title = "Study Group"
+    field_name = "study_group"
+
+
+@admin.register(StudyGroupType)
+class StudyGroupTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "date_created", "date_updated")
+    list_display_links = ("id",)
+    ordering = ("id",)
+
+
+@admin.register(StudyGroup)
+class StudyGroupAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "type", "is_active", "date_created", "date_updated")
+    list_display_links = ("id",)
+    ordering = ("name",)
+    list_filter = ("is_active",)
+    search_fields = ["name"]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "study_group", "is_active", "date_created", "date_updated")
+    list_display_links = ("id",)
+    ordering = ("study_group", "is_active", "name")
+    list_filter = ("is_active",)
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "study_group", "get_study_group_type", "is_session", "date_start", "date_end",
+        "date_created", "date_updated"
+    )
+    list_display_links = ("id",)
+    ordering = ("study_group", "date_start")
+    list_filter = (StudyGroupFilter, "is_session")
+
+    @admin.display(description=_("Type"))
+    def get_study_group_type(self, obj):
+        return obj.study_group.type
+
+
+@admin.register(HistoryLog)
+class HistoryLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "action", "date_created")
+    ordering = ("-date_created",)
+    list_filter = (("user", admin.RelatedOnlyFieldListFilter), ("date_created", DateFieldListFilter))
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
 
 admin.site.site_title = _("Activity")
 admin.site.site_header = _("Mospolytech Activity")
--- activity/main/models.py
+++ activity/main/models.py
@@ -1,3 +1,139 @@
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class AbstractDatestamp(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_("Date created"))
+    date_updated = models.DateTimeField(auto_now=True, verbose_name=_("Date updated"))
+
+    class Meta:
+        abstract = True
+
+
+class StudyGroupType(AbstractDatestamp):
+    name = models.CharField(
+        max_length=8,
+        unique=True,
+        db_index=True,
+        help_text=_("Name of the type of study group."),
+        verbose_name=_("Name")
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Study group type")
+        verbose_name_plural = _("Study group types")
+
+
+class StudyGroup(AbstractDatestamp):
+    name = models.CharField(
+        max_length=16,
+        help_text=_("Name of the study group."),
+        verbose_name=_("Name")
+    )
+    type = models.ForeignKey(
+        StudyGroupType,
+        on_delete=models.CASCADE,
+        help_text=_("Name of the type of study group."),
+        verbose_name=_("Type")
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text=_("Determines whether an entity is active in the system."),
+        verbose_name=_("Active")
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Study group")
+        verbose_name_plural = _("Study groups")
+
+
+class Student(AbstractDatestamp):
+    name = models.CharField(
+        max_length=128,
+        help_text=_("Full name of the student."),
+        verbose_name=_("Full name")
+    )
+    study_group = models.ForeignKey(
+        StudyGroup,
+        on_delete=models.CASCADE,
+        help_text=_("Name of the study group."),
+        verbose_name=_("Study group")
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text=_("Determines whether an entity is active in the system."),
+        verbose_name=_("Active")
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.study_group})"
+
+    class Meta:
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
+
+
+class Schedule(AbstractDatestamp):
+    study_group = models.ForeignKey(
+        StudyGroup,
+        on_delete=models.CASCADE,
+        help_text=_("Name of the study group."),
+        verbose_name=_("Study group")
+    )
+    is_session = models.BooleanField(
+        default=False,
+        help_text=_("Determines whether the schedule is a session schedule or not."),
+        verbose_name=_("Session")
+    )
+    date_start = models.DateField(
+        help_text=_("The start date of the schedule."),
+        verbose_name=_("Date start")
+    )
+    date_end = models.DateField(
+        help_text=_("The end date of the schedule."),
+        verbose_name=_("Date end")
+    )
+    grid = models.JSONField(
+        help_text=_("Raw schedule grid format."),
+        verbose_name=_("Grid")
+    )
+
+    def __str__(self):
+        return f"{self.study_group} ({self.date_start.strftime('%d.%m.%Y')}-{self.date_end.strftime('%d.%m.%Y')})"
+
+    class Meta:
+        verbose_name = _("Schedule")
+        verbose_name_plural = _("Schedules")
+
+
+class HistoryLog(AbstractDatestamp):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        help_text=_("The user who performed the action."),
+        verbose_name=_("User")
+    )
+    action = models.CharField(
+        max_length=128,
+        help_text=_("Action in the system."),
+        verbose_name=_("Action")
+    )
+
+    def __str__(self):
+        local_date = timezone.localtime(self.date_created)
+        return f"{self.user}" + " " + _("action") + f" ({local_date.strftime('%d.%m.%Y %H:%M')})"
+
+    class Meta:
+        verbose_name = _("History Log")
+        verbose_name_plural = _("History Logs")
+
+
+HistoryLog._meta.get_field("date_created").verbose_name = _("Date")
