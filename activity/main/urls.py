89cf57ac899a3f781348d832b4cdb6228161a1a0
r4nd0lph <r11345roma@yandex.ru>
2023-03-12 14:11:56 +0300

    added favicons

--- activity/main/urls.py
+++ activity/main/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic import RedirectView
+
 from main.views import *
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("main/graphics/favicons/favicon.ico"))),
     path("", Index.as_view(), name="index"),
     path("group_auto_complete/", GroupAutoComplete.as_view(), name="group_auto_complete"),
     path("student_auto_complete/", StudentAutoComplete.as_view(), name="student_auto_complete"),
