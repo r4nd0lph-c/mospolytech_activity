40babfa37dd583fc27dfbbfc2520bfda92697f92
r4nd0lph <r11345roma@yandex.ru>
2023-11-26 17:43:24 +0300

    added logic for generation data for academic year

--- activity/main/views.py
+++ activity/main/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date as ddate
 from django.utils.translation import gettext_lazy as _
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
@@ -153,7 +153,23 @@ def get_schedule(request):
 
 
 def get_year_activity(request):
-    """ returns info about student academic year activity"""
+    """ returns info about student academic year activity """
+
+    def special_days_from_month(start_date_str, end_date_str, weekday_num):
+        """ returns the dates of the specified day of the week in the date range """
+
+        start_date = datetime.strptime(start_date_str, "%d.%m.%Y")
+        end_date = datetime.strptime(end_date_str, "%d.%m.%Y")
+
+        dates_list = []
+        delta = 1
+        while start_date <= end_date:
+            if start_date.weekday() == weekday_num:
+                dates_list.append(start_date.strftime("%d.%m.%Y"))
+                delta = 7
+            start_date += timedelta(days=delta)
+
+        return dates_list
 
     if request.method == "POST":
         study_group = request.POST.get("group", None)
@@ -167,9 +183,87 @@ def get_year_activity(request):
             else:
                 return JsonResponse({"message": "something went wrong!"})
 
+            DP = [ddate(start_year, 9, 1), ddate(start_year + 1, 8, 30)]
+            DM = ddate(start_year + 1, 1, 1)
+
+            # search for all groups in which the student studied for the selected academic year
+            required_groups = []
+            group_history = StudyGroupOld.objects.filter(student=student).order_by("date_end")
+            for group in group_history:
+                if (group.date_start <= DP[0] <= group.date_end) or (
+                        group.date_start >= DP[0] and group.date_end <= DP[1]) or (
+                        group.date_start <= DP[1] <= group.date_end):
+                    required_groups.append(group.study_group.name)
+            ag = student.study_group.name
+            if group_history:
+                ag_ds = group_history[0].date_end
+            else:
+                ag_ds = ddate(int("20" + student.study_group.name[:2]), 9, 1)
+            ag_de = datetime.today().date()
+            if (ag_ds <= DP[0] <= ag_de) or (ag_ds >= DP[0] and ag_de <= DP[1]) or (ag_ds <= DP[1] <= ag_de):
+                required_groups.append(ag)
+
+            # format list to ["g1", "g2"] where "g1" - first semester group, "g2" - seconds semester group
+            if len(required_groups) > 2:
+                required_groups = required_groups[:2]
+            if len(required_groups) < 2:
+                required_groups.append(required_groups[0])
+
+            # find required schedules
+            schedules = [
+                Schedule.objects.filter(
+                    study_group__name=required_groups[0],
+                    date_start__gte=DP[0],
+                    date_end__lte=DM).first(),
+                Schedule.objects.filter(
+                    study_group__name=required_groups[1],
+                    date_start__gte=DM,
+                    date_end__lte=DP[1]).first()
+            ]
+
+            # find subjects
+            subjects_filter = []
+            subjects = []
+            semester = 0
+            for schedule in schedules:
+                semester += 1
+                if schedule is not None:
+                    day_num = -1
+                    for day in schedule.grid:
+                        day_num += 1
+                        for section in day:
+                            for sbj in section:
+                                if sbj["rooms"]:
+                                    if sbj["title"] not in subjects_filter:
+                                        subjects_filter.append(sbj["title"])
+                                        visit_dates = special_days_from_month(sbj["dates"][0], sbj["dates"][1], day_num)
+                                        subjects.append({
+                                            "title": sbj["title"],
+                                            "group": schedule.study_group.name,
+                                            "semester": semester,
+                                            "dates": sbj["dates"],
+                                            "year": start_year + semester - 1,
+                                            # TODO: fill "visits" status
+                                            "visits": [{"date": d, "status": None} for d in visit_dates]
+                                        })
+
+            # format "activity" results
             activity = {
-                "years": [start_year, start_year + 1]
+                "years": [start_year, start_year + 1],
+                "semesters": {
+                    1: {
+                        "group": required_groups[0],
+                        "date_start": schedules[0].date_start.strftime("%d.%m.%Y"),
+                        "date_end": schedules[0].date_end.strftime("%d.%m.%Y")
+                    } if schedules[0] else None,
+                    2: {
+                        "group": required_groups[1],
+                        "date_start": schedules[1].date_start.strftime("%d.%m.%Y"),
+                        "date_end": schedules[1].date_end.strftime("%d.%m.%Y")
+                    } if schedules[1] else None},
+                "subjects": subjects
             }
+
             return JsonResponse({
                 "student": {"name": name, "group": study_group},
                 "activity": activity
